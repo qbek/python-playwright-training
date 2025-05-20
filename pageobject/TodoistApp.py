51ad00691e847b9388c1a21b1f05fafa89bc106e
Jakub Szewczyk <jakub.szewczyk@hotmail.com>
2025-05-20 11:12:42 +0200

    Test setup using fixtures

--- pageobject/TodoistApp.py
+++ pageobject/TodoistApp.py
@@ -1,11 +1,15 @@
+_LOGIN_PAGE_URL = 'https://app.todoist.com/auth/login'
+_MAIN_APP_URL = 'https://app.todoist.com/app/'
+
+
 class TodoistApp:
 
   def __init__(self, page):
     self._page = page
 
   def navigateToLoginPage(self):
-    self._page.goto('https://app.todoist.com/auth/login')
-  
+    self._page.goto(_LOGIN_PAGE_URL)
+
   def getAllCookies(self):
-    self._page.wait_for_url('https://app.todoist.com/app/')
+    self._page.wait_for_url(_MAIN_APP_URL)
     return self._page.context.cookies()
--- todoistLoginTest.py
+++ todoistLoginTest.py
@@ -4,15 +4,25 @@ from pageobject.TodoistApp import TodoistApp
 
 from playwright.sync_api import Page, expect, Playwright
 
-def test_userCanLogIn(page: Page):
-  todoist = TodoistApp(page)
-  login = LoginForm(page)
+@pytest.fixture
+def login(page: Page):
+  return LoginForm(page)
+
+@pytest.fixture
+def todoist(page: Page):
+  return TodoistApp(page)
+
+
+def test_userCanLogIn(login, todoist):
+  #Given
   todoist.navigateToLoginPage()
 
+  #When
   login.enterEmail('gbinxeqerpnywwysux@awdrt.org')
   login.enterPassword('ti4FCvBL39i7mMq')
   login.submitForm()
 
+  #Then
   cookies = todoist.getAllCookies()
   assert any(cookie['name'] == 'todoistd' for cookie in cookies), f"Expected cookie todoistd dosn't exists in: {cookies}"
 
